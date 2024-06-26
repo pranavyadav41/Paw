be656c14e6c141dfac3e0db9de5e69a25e6ba77a
Pranav Yadav <akpranavyadav@gmail.com>
2024-06-26 21:10:50 +0530

    added paypal id

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,7 @@ import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 
-const CLIENT_ID =import.meta.env.VITE_PAYPAL_CLIENT_ID;
+const CLIENT_ID ="AQfGdzRqN2AI8KMjftw1H6GyxshTx0QxieZ3oELTUfN0qH-1F5zCofB6GHtu0G6rxhGT8Kgg6MYyF8IW";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
