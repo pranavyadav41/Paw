ae55fafdb4e7df1b873d88bbf70c54045515ec66
Pranav Yadav <akpranavyadav@gmail.com>
2024-06-26 23:40:24 +0530

    few changes

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import errorHandle from "../api/error";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = "https://soundmagic.fun/api";
 
 const Api = axios.create({
   baseURL: BASE_URL, 
