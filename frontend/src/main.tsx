7a304d9e5d58dacb4f2e4fc14c27d5f94a10ed4d
Pranav Yadav <akpranavyadav@gmail.com>
2024-06-29 22:24:19 +0530

    google fixed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,7 +11,7 @@ import "./index.css";
 
 const CLIENT_ID ="AQfGdzRqN2AI8KMjftw1H6GyxshTx0QxieZ3oELTUfN0qH-1F5zCofB6GHtu0G6rxhGT8Kgg6MYyF8IW";
 
-const GOOGLE_ID=import.meta.env.VITE_G_CLIENT_ID
+const GOOGLE_ID="337606298738-1700n518dumqome58ngcvmfarrsate57.apps.googleusercontent.com"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
